10849fa520e6ee65ec911d288e66eebb43625174
Viktor Savonin <viktor.savonin@meawallet.com>
2019-07-19 09:27:11 +0300

    Delete CoursePaymentTemplate backend and integration test

--- src/main/java/lv/javaguru/cms/rest/controllers/course/CoursePaymentTemplateController.java
+++ src/main/java/lv/javaguru/cms/rest/controllers/course/CoursePaymentTemplateController.java
@@ -1,12 +1,16 @@
 package lv.javaguru.cms.rest.controllers.course;
 
+import lv.javaguru.cms.rest.controllers.course.model.paymenttemplate.DeleteCoursePaymentTemplateRequest;
+import lv.javaguru.cms.rest.controllers.course.model.paymenttemplate.DeleteCoursePaymentTemplateResponse;
 import lv.javaguru.cms.rest.controllers.course.model.paymenttemplate.GetCoursePaymentTemplateRequest;
 import lv.javaguru.cms.rest.controllers.course.model.paymenttemplate.GetCoursePaymentTemplateResponse;
 import lv.javaguru.cms.rest.controllers.course.model.paymenttemplate.UpdateCoursePaymentTemplateRequest;
 import lv.javaguru.cms.rest.controllers.course.model.paymenttemplate.UpdateCoursePaymentTemplateResponse;
+import lv.javaguru.cms.services.course.paymenttemplates.DeleteCoursePaymentTemplateService;
 import lv.javaguru.cms.services.course.paymenttemplates.GetCoursePaymentTemplateService;
 import lv.javaguru.cms.services.course.paymenttemplates.UpdateCoursePaymentTemplateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,11 +26,12 @@ public class CoursePaymentTemplateController {
 
     @Autowired private UpdateCoursePaymentTemplateService updateCoursePaymentTemplateService;
     @Autowired private GetCoursePaymentTemplateService getCoursePaymentTemplateService;
+    @Autowired private DeleteCoursePaymentTemplateService deleteCoursePaymentTemplateService;
 
     @PutMapping(path = "/course/{courseId}/paymentTemplate", produces = "application/json")
-    public UpdateCoursePaymentTemplateResponse updatePaymentTemplate(@PathVariable("courseId") Long courseId,
-                                                                     @Valid @RequestBody UpdateCoursePaymentTemplateRequest request,
-                                                                     Principal principal) {
+    public UpdateCoursePaymentTemplateResponse update(@PathVariable("courseId") Long courseId,
+                                                      @Valid @RequestBody UpdateCoursePaymentTemplateRequest request,
+                                                      Principal principal) {
         if (!Objects.equals(courseId, request.getCourseId())) {
             throw new IllegalArgumentException("courseId");
         }
@@ -34,10 +39,20 @@ public class CoursePaymentTemplateController {
         return updateCoursePaymentTemplateService.update(request);
     }
 
+    @DeleteMapping(path = "/course/{courseId}/paymentTemplate", produces = "application/json")
+    public DeleteCoursePaymentTemplateResponse delete(@PathVariable("courseId") Long courseId,
+                                                      Principal principal) {
+        DeleteCoursePaymentTemplateRequest request = DeleteCoursePaymentTemplateRequest.builder()
+                .courseId(courseId)
+                .build();
+        request.setSystemUserLogin(principal.getName());
+        return deleteCoursePaymentTemplateService.delete(request);
+    }
+
     @GetMapping(path = "/course/{courseId}/paymentTemplate/{paymentTemplateId}", produces = "application/json")
-    public GetCoursePaymentTemplateResponse getPaymentTemplate(@PathVariable("courseId") Long courseId,
-                                                               @PathVariable("paymentTemplateId") Long paymentTemplateId,
-                                                               Principal principal) {
+    public GetCoursePaymentTemplateResponse get(@PathVariable("courseId") Long courseId,
+                                                @PathVariable("paymentTemplateId") Long paymentTemplateId,
+                                                Principal principal) {
         GetCoursePaymentTemplateRequest request = GetCoursePaymentTemplateRequest.builder()
                 .courseId(courseId)
                 .paymentTemplateId(paymentTemplateId)
@@ -45,10 +60,5 @@ public class CoursePaymentTemplateController {
         request.setSystemUserLogin(principal.getName());
         return getCoursePaymentTemplateService.update(request);
     }
-
-
-
-    // DELETE Course Payment Template
-
 
 }
