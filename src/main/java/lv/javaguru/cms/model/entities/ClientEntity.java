3040343f6e1932c47b326ca00a687f048905d363
Viktor Savonin <viktor.savonin@meawallet.com>
2019-08-10 10:37:06 +0300

    Client properties update

--- src/main/java/lv/javaguru/cms/model/entities/ClientEntity.java
+++ src/main/java/lv/javaguru/cms/model/entities/ClientEntity.java
@@ -24,13 +24,13 @@ public class ClientEntity extends BaseEntity {
     @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @Column(name = "last_name")
+    @Column(name = "last_name", nullable = false)
     private String lastName;
 
-    @Column(name = "phone_number")
+    @Column(name = "phone_number", nullable = false)
     private String phoneNumber;
 
-    @Column(name = "email")
+    @Column(name = "email", nullable = false)
     private String email;
 
     @Column(name = "promo_code")
